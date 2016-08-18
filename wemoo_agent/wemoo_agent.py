fc6fe0a748b03f6f65a6a91cbf8b54f89b5e74e4
Shawn Tien <hightian@gmail.com>
2016-08-18 21:37:18 +0800

    fix exit bug

--- wemoo_agent/wemoo_agent.py
+++ wemoo_agent/wemoo_agent.py
@@ -4,33 +4,59 @@
 
 import os
 import sys
+import traceback
 
 from daemonize import Daemonize
 
 from wemoo_agent.config.config import config
 from wemoo_agent.core.hosts import login_or_register_host
 from wemoo_agent.core.produce import Eventloop
+from wemoo_agent.system import shell
+
+HELP = """
+
+Run command like:
+$ wemoo start|stop|run
+
+DESCRIPTION:
+- start\tStart daemon;
+- stop\tStop daemon process running in the background;
+- run\tRun in terminal instesd of background.
+"""
 
 
 def run():
+    # register device
+    if not login_or_register_host():
+        sys.exit("Regist failed.")
+
     event = Eventloop(config)
     event.event_loop()
 
 
 def main():
-    # register device
-    if not login_or_register_host():
-        sys.exit("Regist failed.")
-    print('Register host.')
+    global HELP
+    if len(sys.argv) < 2:
+        sys.exit('Parameter is missing.' + HELP)
 
     # whether daemon or not
-    if config.debug:
-        run()
-    else:
+    try:
+        action = sys.argv[1]
         daemon = Daemonize(app="Wemoo Agent", pid=config.pid_file, action=run)
-        if os.path.isfile(config.pid_file):
-            print('Exit daemon.')
-            daemon.exit()
-        else:
-            print('Start daemon.')
+
+        if action == 'run':
+            run()
+        elif action == 'start':
             daemon.start()
+            print('Start daemon.')
+        elif action == 'stop':
+            result = shell.exec_shell_script('kill `cat ' + config.pid_file + '`')
+            if (result == b''):
+                print('Exit daemon.')
+            else:
+                print('Fail to stop.')
+    except KeyboardInterrupt:
+        print("\nExited.")
+    except Exception as inst:
+        print('Launch failed.')
+        traceback.print_exc()
