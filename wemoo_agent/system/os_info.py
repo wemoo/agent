d729f62fe7398d4bf8f46b312dd8475cbe574ad7
Shawn Tien <hightian@gmail.com>
2016-09-03 16:10:08 +0800

    add system

--- wemoo_agent/system/os_info.py
+++ wemoo_agent/system/os_info.py
@@ -4,13 +4,6 @@ import platform
 from wemoo_agent.system.shell import exec_shell_script
 
 uname = platform.uname()
-system = {'system': uname.system,
-          'node': uname.node,
-          'release': uname.release,
-          'version': uname.version,
-          'machine': uname.machine,
-          'processor': uname.processor,
-          'uuid': os_uuid()}
 
 
 def os_uuid():
@@ -18,11 +11,11 @@ def os_uuid():
     script = None
 
     if os == 'Linux':
-        script = r"""
+        script = """
         cat /etc/machine-id
         """
     elif os == 'Darwin':
-        script = r"""
+        script = """
         system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'
         """
 
@@ -30,4 +23,15 @@ def os_uuid():
     if not output:
         return None
 
-    return ''.join(output.split())
+    return output.decode("utf-8").strip('\n')
+
+
+system = {
+    'system': uname.system,
+    'node': uname.node,
+    'release': uname.release,
+    'version': uname.version,
+    'machine': uname.machine,
+    'processor': uname.processor,
+    'uuid': os_uuid()
+}
